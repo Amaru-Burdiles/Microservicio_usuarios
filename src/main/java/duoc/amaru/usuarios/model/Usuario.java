ba15eae4a2932bf80b2677f6fcb2435187fd1606
Amaru Burdiles <umarurama2@gmail.com>
2026-05-15 16:03:53 -0400

    Pequeño error arreglado     - Se elimino la anotacion @AllArgsConstructor de lombok       dado que el contructor completo fue definido manualmente.

--- src/main/java/duoc/amaru/usuarios/model/Usuario.java
+++ src/main/java/duoc/amaru/usuarios/model/Usuario.java
@@ -9,12 +9,10 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "usuario")
